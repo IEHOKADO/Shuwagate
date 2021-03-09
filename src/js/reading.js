845f4b732668112a644f0e8be953f312f44d24f1
DAO <58584836+IEHOKADO@users.noreply.github.com>
2021-03-10 01:44:30 +0900

    Add files via upload

--- src/js/reading.js
+++ src/js/reading.js
@@ -1,176 +1,172 @@
 const level = location.search.slice(7);  //難易度をURLパラメータから取得
 
-let vocab;  //問題の単語
-let exp;  //クリア時にもらえるレベル
+let vocab;  // 問題の単語
 
-if(level == '1') {
-	vocab = [['あか','../video/reading_level1/aka.mp4'],
-			['あさ','../video/reading_level1/asa.mp4'],
-			['いぬ','../video/reading_level1/inu.mp4'],
-			['うし','../video/reading_level1/ushi.mp4'],
-			['えき','../video/reading_level1/eki.mp4'],
-			['かみ','../video/reading_level1/kami.mp4'],
-			['きく','../video/reading_level1/kiku.mp4'],
-			['こま','../video/reading_level1/koma.mp4'],
-			['さる','../video/reading_level1/saru.mp4'],
-			['しか','../video/reading_level1/shika.mp4'],
-			['すし','../video/reading_level1/sushi.mp4'],
-			['せみ','../video/reading_level1/semi.mp4'],
-			['そら','../video/reading_level1/sora.mp4'],
-			['たき','../video/reading_level1/taki.mp4'],
-			['ちち','../video/reading_level1/titi.mp4'],
-			['つき','../video/reading_level1/tsuki.mp4'],
-			['てら','../video/reading_level1/tera.mp4'],
-			['とり','../video/reading_level1/tori.mp4'],
-			['なみ','../video/reading_level1/nami.mp4'],
-			['にく','../video/reading_level1/niku.mp4'],
-			['ぬま','../video/reading_level1/numa.mp4'],
-			['ねこ','../video/reading_level1/neko.mp4'],
-			['のり','../video/reading_level1/nori.mp4'],
-			['はし','../video/reading_level1/hashi.mp4'],
-			['ひな','../video/reading_level1/hina.mp4'],
-			['ふね','../video/reading_level1/hune.mp4'],
-			['へり','../video/reading_level1/heri.mp4'],
-			['ほん','../video/reading_level1/honn.mp4'],
-			['まり','../video/reading_level1/mari.mp4'],
-			['みの','../video/reading_level1/mino.mp4'],
-			['むし','../video/reading_level1/mushi.mp4'],
-			['めま','../video/reading_level1/numa.mp4'],
-			['もも','../video/reading_level1/momo.mp4'],
-			['やり','../video/reading_level1/yari.mp4'],
-			['ゆみ','../video/reading_level1/yumi.mp4'],
-			['よる','../video/reading_level1/yoru.mp4'],
-			['らめ','../video/reading_level1/rame.mp4'],
-			['わに','../video/reading_level1/wani.mp4'],
-			['もり','../video/reading_level1/mori.mp4'],
-			['かに','../video/reading_level1/kani.mp4'],
-			['りす','../video/reading_level1/risu.mp4'],
-			['おか','../video/reading_level1/oka.mp4'],
-			['いす','../video/reading_level1/isu.mp4'],
-			['うま','../video/reading_level1/uma.mp4'],
-			['やま','../video/reading_level1/yama.mp4']];
-	exp = 1;
+if (level == '1') {
+	vocab = [['あか', 'reading_level1/aka'],
+	['あさ', 'reading_level1/asa'],
+	['いぬ', 'reading_level1/inu'],
+	['うし', 'reading_level1/ushi'],
+	['えき', 'reading_level1/eki'],
+	['かみ', 'reading_level1/kami'],
+	['きく', 'reading_level1/kiku'],
+	['こま', 'reading_level1/koma'],
+	['さる', 'reading_level1/saru'],
+	['しか', 'reading_level1/shika'],
+	['すし', 'reading_level1/sushi'],
+	['せみ', 'reading_level1/semi'],
+	['そら', 'reading_level1/sora'],
+	['たき', 'reading_level1/taki'],
+	['ちち', 'reading_level1/titi'],
+	['つき', 'reading_level1/tsuki'],
+	['てら', 'reading_level1/tera'],
+	['とり', 'reading_level1/tori'],
+	['なみ', 'reading_level1/nami'],
+	['にく', 'reading_level1/niku'],
+	['ぬま', 'reading_level1/numa'],
+	['ねこ', 'reading_level1/neko'],
+	['のり', 'reading_level1/nori'],
+	['はし', 'reading_level1/hashi'],
+	['ひな', 'reading_level1/hina'],
+	['ふね', 'reading_level1/hune'],
+	['へり', 'reading_level1/heri'],
+	['ほん', 'reading_level1/honn'],
+	['まり', 'reading_level1/mari'],
+	['みの', 'reading_level1/mino'],
+	['むし', 'reading_level1/mushi'],
+	['めま', 'reading_level1/numa'],
+	['もも', 'reading_level1/momo'],
+	['やり', 'reading_level1/yari'],
+	['ゆみ', 'reading_level1/yumi'],
+	['よる', 'reading_level1/yoru'],
+	['らめ', 'reading_level1/rame'],
+	['わに', 'reading_level1/wani'],
+	['もり', 'reading_level1/mori'],
+	['かに', 'reading_level1/kani'],
+	['りす', 'reading_level1/risu'],
+	['おか', 'reading_level1/oka'],
+	['いす', 'reading_level1/isu'],
+	['うま', 'reading_level1/uma'],
+	['やま', 'reading_level1/yama']];
 }
 
-else if(level == '2') {
-	vocab = [['あんこ','../video/reading_level2/anko.mp4'],
-			['いるか','../video/reading_level2/iruka.mp4']
-			['うさぎ','../video/reading_level2/usagi.mp4'],
-			['うなぎ','../video/reading_level2/unagi.mp4'],
-			['えのぐ','../video/reading_level2/enogu.mp4'],
-			['おうむ','../video/reading_level2/oumu.mp4'],
-			['からす','../video/reading_level2/karasu.mp4'],
-			['きりん','../video/reading_level2/kirin.mp4'],
-			['くるみ','../video/reading_level2/kurumi.mp4'],
-			['けいと','../video/reading_level2/keito.mp4'],
-			['こあら','../video/reading_level2/koara.mp4'],
-			['さくら','../video/reading_level2/sakura.mp4'],
-			['しまね','../video/reading_level2/shimane.mp4'],
-			['すみれ','../video/reading_level2/sumire.mp4'],
-			['せなか','../video/reading_level2/senaka.mp4'],
-			['そふと','../video/reading_level2/sohuto.mp4'],
-			['たぬき','../video/reading_level2/tanuki.mp4'],
-			['ちくわ','../video/reading_level2/tikuwa.mp4'],
-			['つみき','../video/reading_level2/tumiki.mp4'],
-			['てにす','../video/reading_level2/tenisu.mp4'],
-			['といれ','../video/reading_level2/toire.mp4'],
-			['なみだ','../video/reading_level2/namida.mp4'],
-			['にしん','../video/reading_level2/nishin.mp4'],
-			['ぬりえ','../video/reading_level2/nurie.mp4'],
-			['ねずみ','../video/reading_level2/nezumi.mp4'],
-			['のーと','../video/reading_level2/no-to.mp4'],
-			['はなし','../video/reading_level2/hanashi.mp4'],
-			['ぷりん','../video/reading_level2/purin.mp4'],
-			['へちま','../video/reading_level2/hetima.mp4'],
-			['ほらー','../video/reading_level2/hora-.mp4'],
-			['まっと','../video/reading_level2/matto.mp4'],
-			['むすめ','../video/reading_level2/musume.mp4'],
-			['めろん','../video/reading_level2/meron.mp4'],
-			['もみじ','../video/reading_level2/momizi.mp4'],
-			['やすみ','../video/reading_level2/yasumi.mp4'],
-			['ゆみや','../video/reading_level2/yumiya.mp4'],
-			['よっと','../video/reading_level2/yotto.mp4'],
-			['らっこ','../video/reading_level2/rakko.mp4'],
-			['わいん','../video/reading_level2/wain.mp4'],
-			['きつね','../video/reading_level2/kitune.mp4'],
-			['かばん','../video/reading_level2/kaban.mp4'],
-			['れもん','../video/reading_level2/remon.mp4'],
-			['ろしあ','../video/reading_level2/roshia.mp4']];
-	exp = 2;
+else if (level == '2') {
+	vocab = [['あんこ', 'reading_level2/anko'],
+	['いるか', 'reading_level2/iruka']
+	['うさぎ', 'reading_level2/usagi'],
+	['うなぎ', 'reading_level2/unagi'],
+	['えのぐ', 'reading_level2/enogu'],
+	['おうむ', 'reading_level2/oumu'],
+	['からす', 'reading_level2/karasu'],
+	['きりん', 'reading_level2/kirin'],
+	['くるみ', 'reading_level2/kurumi'],
+	['けいと', 'reading_level2/keito'],
+	['こあら', 'reading_level2/koara'],
+	['さくら', 'reading_level2/sakura'],
+	['しまね', 'reading_level2/shimane'],
+	['すみれ', 'reading_level2/sumire'],
+	['せなか', 'reading_level2/senaka'],
+	['そふと', 'reading_level2/sohuto'],
+	['たぬき', 'reading_level2/tanuki'],
+	['ちくわ', 'reading_level2/tikuwa'],
+	['つみき', 'reading_level2/tumiki'],
+	['てにす', 'reading_level2/tenisu'],
+	['といれ', 'reading_level2/toire'],
+	['なみだ', 'reading_level2/namida'],
+	['にしん', 'reading_level2/nishin'],
+	['ぬりえ', 'reading_level2/nurie'],
+	['ねずみ', 'reading_level2/nezumi'],
+	['のーと', 'reading_level2/no-to'],
+	['はなし', 'reading_level2/hanashi'],
+	['ぷりん', 'reading_level2/purin'],
+	['へちま', 'reading_level2/hetima'],
+	['ほらー', 'reading_level2/hora-'],
+	['まっと', 'reading_level2/matto'],
+	['むすめ', 'reading_level2/musume'],
+	['めろん', 'reading_level2/meron'],
+	['もみじ', 'reading_level2/momizi'],
+	['やすみ', 'reading_level2/yasumi'],
+	['ゆみや', 'reading_level2/yumiya'],
+	['よっと', 'reading_level2/yotto'],
+	['らっこ', 'reading_level2/rakko'],
+	['わいん', 'reading_level2/wain'],
+	['きつね', 'reading_level2/kitune'],
+	['かばん', 'reading_level2/kaban'],
+	['れもん', 'reading_level2/remon'],
+	['ろしあ', 'reading_level2/roshia']];
 }
 
-else if(level == '3') {
-	vocab = [['あめんぼ','../video/reading_level3/amenbo.mp4'],
-			['いめーじ','../video/reading_level3/ime-zi.mp4']
-			['うらぎり','../video/reading_level3/uragiri.mp4'],
-			['えたのーる','../video/reading_level3/etano-ru.mp4'],
-			['おんがく','../video/reading_level3/ongaku.mp4'],
-			['かっぱ','../video/reading_level3/kappa.mp4'],
-			['きおくりょく','../video/reading_level3/kiokuryoku.mp4'],
-			['くるーざー','../video/reading_level3/kuru-za-.mp4'],
-			['けいこうとう','../video/reading_level3/keikoutou.mp4'],
-			['こっぱみじん','../video/reading_level3/koppamizin.mp4'],
-			['さいきょう','../video/reading_level3/saikyou.mp4'],
-			['しっぱい','../video/reading_level3/sippai.mp4'],
-			['すいぞくかん','../video/reading_level3/suizokukan.mp4'],
-			['せんぱい','../video/reading_level3/senpai.mp4'],
-			['そふとうぇあ','../video/reading_level3/sohutoulea.mp4'],
-			['たっきゅう','../video/reading_level3/takkyuu.mp4'],
-			['ちぇっく','../video/reading_level3/tyekku.mp4'],
-			['ついったー','../video/reading_level3/tuitta-.mp4'],
-			['てぃっしゅ','../video/reading_level3/telissyu.mp4'],
-			['とっぷ','../video/reading_level3/toppu.mp4'],
-			['なぞとき','../video/reading_level3/nazotoki.mp4'],
-			['にんじん','../video/reading_level3/ninnzin.mp4'],
-			['ぬいぐるみ','../video/reading_level3/nuigurumi.mp4'],
-			['ねがてぃぶ','../video/reading_level3/negatelibu.mp4'],
-			['のうぎょう','../video/reading_level3/nougyou.mp4'],
-			['はいきんぐ','../video/reading_level3/haikinngu.mp4'],
-			['ひえしょう','../video/reading_level3/hiesyou.mp4'],
-			['ぷらすちっく','../video/reading_level3/purasutikku.mp4'],
-			['べんきょう','../video/reading_level3/bennkyou.mp4'],
-			['ほうこくしょ','../video/reading_level3/houkokusyo.mp4'],
-			['まかろん','../video/reading_level3/makaron.mp4'],
-			['みずたまり','../video/reading_level3/mizutamari.mp4'],
-			['むーびー','../video/reading_level3/mu-bi-.mp4'],
-			['めいきんぐ','../video/reading_level3/meikingu.mp4'],
-			['もぎてん','../video/reading_level3/mogiten.mp4'],
-			['やじるし','../video/reading_level3/yazirusi.mp4'],
-			['ゆうえんち','../video/reading_level3/yuuenti.mp4'],
-			['ようきゅう','../video/reading_level3/youkyuu.mp4'],
-			['らっぱ','../video/reading_level3/rappa.mp4'],
-			['りあくしょん','../video/reading_level3/riakusyon.mp4'],
-			['るすばん','../video/reading_level3/rusuban.mp4'],
-			['れんたかー','../video/reading_level3/rentaka-.mp4'],
-			['わいふぁい','../video/reading_level3/waifai.mp4'],
-			['ばらんす','../video/reading_level3/baransu.mp4'],
-			['ぱぷりか','../video/reading_level3/papurika.mp4'],
-			['ぴくにっく','../video/reading_level3/pikunikku.mp4'],
-			['ぺっとぼとる','../video/reading_level3/pettobotoru.mp4']];
-	exp = 3;
+else if (level == '3') {
+	vocab = [['あめんぼ', 'reading_level3/amenbo'],
+	['いめーじ', 'reading_level3/ime-zi']
+	['うらぎり', 'reading_level3/uragiri'],
+	['えたのーる', 'reading_level3/etano-ru'],
+	['おんがく', 'reading_level3/ongaku'],
+	['かっぱ', 'reading_level3/kappa'],
+	['きおくりょく', 'reading_level3/kiokuryoku'],
+	['くるーざー', 'reading_level3/kuru-za-'],
+	['けいこうとう', 'reading_level3/keikoutou'],
+	['こっぱみじん', 'reading_level3/koppamizin'],
+	['さいきょう', 'reading_level3/saikyou'],
+	['しっぱい', 'reading_level3/sippai'],
+	['すいぞくかん', 'reading_level3/suizokukan'],
+	['せんぱい', 'reading_level3/senpai'],
+	['そふとうぇあ', 'reading_level3/sohutoulea'],
+	['たっきゅう', 'reading_level3/takkyuu'],
+	['ちぇっく', 'reading_level3/tyekku'],
+	['ついったー', 'reading_level3/tuitta-'],
+	['てぃっしゅ', 'reading_level3/telissyu'],
+	['とっぷ', 'reading_level3/toppu'],
+	['なぞとき', 'reading_level3/nazotoki'],
+	['にんじん', 'reading_level3/ninnzin'],
+	['ぬいぐるみ', 'reading_level3/nuigurumi'],
+	['ねがてぃぶ', 'reading_level3/negatelibu'],
+	['のうぎょう', 'reading_level3/nougyou'],
+	['はいきんぐ', 'reading_level3/haikinngu'],
+	['ひえしょう', 'reading_level3/hiesyou'],
+	['ぷらすちっく', 'reading_level3/purasutikku'],
+	['べんきょう', 'reading_level3/bennkyou'],
+	['ほうこくしょ', 'reading_level3/houkokusyo'],
+	['まかろん', 'reading_level3/makaron'],
+	['みずたまり', 'reading_level3/mizutamari'],
+	['むーびー', 'reading_level3/mu-bi-'],
+	['めいきんぐ', 'reading_level3/meikingu'],
+	['もぎてん', 'reading_level3/mogiten'],
+	['やじるし', 'reading_level3/yazirusi'],
+	['ゆうえんち', 'reading_level3/yuuenti'],
+	['ようきゅう', 'reading_level3/youkyuu'],
+	['らっぱ', 'reading_level3/rappa'],
+	['りあくしょん', 'reading_level3/riakusyon'],
+	['るすばん', 'reading_level3/rusuban'],
+	['れんたかー', 'reading_level3/rentaka-'],
+	['わいふぁい', 'reading_level3/waifai'],
+	['ばらんす', 'reading_level3/baransu'],
+	['ぱぷりか', 'reading_level3/papurika'],
+	['ぴくにっく', 'reading_level3/pikunikku'],
+	['ぺっとぼとる', 'reading_level3/pettobotoru']];
 }
 
-else location.href = 'index.html';  //URLがおかしい場合はメニューにとぶ
+else location.href = 'dashboard.html';  //URLがおかしい場合はメニューにとぶ
 
 let status = true;  //処理が可能か判断する
 let score = 0;  //正解数
 let ram = Math.floor(Math.random() * Math.floor(vocab.length));  //乱数の生成
 let cpText = vocab[ram][0];  //テキストはランダムで決める
 //console.log(cpText);
-$("#video").html('<video controls src="' + vocab[ram][1] + '" type="video/mp4" width="560" height="420"></video>');
+$("#video").html('<video controls src="../video/' + vocab[ram][1] + '.mp4" type="video/mp4" width="800px" height="600px"></video>');
 function reply() {
-	if(status) {
+	if (status) {
 		let usrText = $("#textInput").val();  //ユーザーのテキスト
 		$("#textInput").val("");  //入力フォームを空にする
-		if(usrText == cpText) {
+		if (usrText == cpText) {
 			score++;
-            if(score == 3) {
+			if (score == 3) {
 				status = false;  //ユーザーからの入力を終了する
 				swal.fire({
 					title: "レッスンクリア！",
 					icon: "success",
-				}).then(function() {
-					location.href = 'index.html';
+				}).then(function () {
+					location.href = 'dashboard.html';
 				});
 			}
 			else {
@@ -182,8 +178,7 @@ function reply() {
 				});
 				ram = Math.floor(Math.random() * Math.floor(vocab.length));
 				cpText = vocab[ram][0];  //テキストはランダムで決める
-				//console.log(cpText);
-				$("#video").html('<video controls src="' + vocab[ram][1] + '" type="video/mp4" width="560" height="420"></video>');
+				$("#video").html('<video controls src="../video/' + vocab[ram][1] + '.mp4" type="video/mp4" width="800px" height="600px"></video>');
 			}
 		}
 		else {
@@ -197,18 +192,18 @@ function reply() {
 	}
 }
 
-$("#textInput").keypress(function(e) {
-    if ((e.which == 13) && document.getElementById("textInput").value != "" ) reply();
+$("#textInput").keypress(function (e) {
+	if ((e.which == 13) && document.getElementById("textInput").value != "") reply();
 })
 
-$("#buttonInput").click(function() {
-    if (document.getElementById("textInput").value != "") reply();
+$("#buttonInput").click(function () {
+	if (document.getElementById("textInput").value != "") reply();
 })
 
-$("#yubimoji").click(function() {
-	$("#modal").fadeIn();  //モーダルを表示
+$("#yubimoji").click(function () {
+	$(".modal").fadeIn();  // モーダルを表示
 })
 
-$(".modal-close").click(function() {
-	$("#modal").fadeOut();  //モーダルを非表示
+$(".modal-close").click(function () {
+	$(".modal").fadeOut();  // モーダルを非表示
 })
--- src/js/speaking.js
+++ src/js/speaking.js
@@ -1,83 +1,86 @@
 const level = location.search.slice(7);  //難易度をURLパラメータから取得
 
-const CLASSES = {0:'あ', 1:'い', 2:'う', 3:'え', 4:'お',
-				 5:'か', 6:'き', 7:'く', 8:'け', 9:'こ',
-				 10:'さ', 11:'し', 12:'す', 13:'せ', 14:'そ',
-				 15:'た', 16:'ち', 17:'つ', 18:'て', 19:'と',
-				 20:'な', 21:'に', 22:'ぬ', 23:'ね', 
-				 24:'は', 25:'ひ', 26:'ふ', 27:'へ', 28:'ほ',
-				 29:'ま', 30:'み', 31:'む', 32:'め', 33:'も',
-				 34:'や', 35:'ゆ', 36:'よ',
-				 37:'ら', 38:'る', 39:'れ', 40:'ろ',
-				 41:'わ'}
+const CLASSES = {
+	0: 'あ', 1: 'い', 2: 'う', 3: 'え', 4: 'お',
+	5: 'か', 6: 'き', 7: 'く', 8: 'け', 9: 'こ',
+	10: 'さ', 11: 'し', 12: 'す', 13: 'せ', 14: 'そ',
+	15: 'た', 16: 'ち', 17: 'つ', 18: 'て', 19: 'と',
+	20: 'な', 21: 'に', 22: 'ぬ', 23: 'ね',
+	24: 'は', 25: 'ひ', 26: 'ふ', 27: 'へ', 28: 'ほ',
+	29: 'ま', 30: 'み', 31: 'む', 32: 'め', 33: 'も',
+	34: 'や', 35: 'ゆ', 36: 'よ',
+	37: 'ら', 38: 'る', 39: 'れ', 40: 'ろ',
+	41: 'わ'
+}
 
-let vocab;  //問題の単語
+let vocab;  // 問題の単語
 
-if(level == '1') {
+if (level == '1') {
 	vocab = [
-		'あめ','いぬ','うし','えき','おか',
-		'かみ','きく','くせ','けさ','こま',
-		'さる','しか','すし','せみ','そら',
-		'たき','つき','てら','とし',
-		'なみ','にく','ぬま','ねこ',
-		'はし','ひな','ふね','へた','ほか',
-		'また','みき','むし','めか','もか',
-		'やま','ゆみ','よる','らめ','るす','れあ','ろか',
+		'あめ', 'いぬ', 'うし', 'えき', 'おか',
+		'かみ', 'きく', 'くせ', 'けさ', 'こま',
+		'さる', 'しか', 'すし', 'せみ', 'そら',
+		'たき', 'つき', 'てら', 'とし',
+		'なみ', 'にく', 'ぬま', 'ねこ',
+		'はし', 'ひな', 'ふね', 'へた', 'ほか',
+		'また', 'みき', 'むし', 'めか', 'もか',
+		'やま', 'ゆみ', 'よる', 'らめ', 'るす', 'れあ', 'ろか',
 		'わに'
 	];
 }
 
-else if(level == '2') {
+else if (level == '2') {
 	vocab = [
-		'あした','いるか','うしろ','えすて','おうむ',
-		'からす','きつね','くるみ','けいと','こあら',
-		'さくら','しまね','すみれ','せなか','そふと',
-		'たぬき','ちくわ','つみき','てにす','といれ',
-		'なかみ','にもつ','ぬるい','ねむい',
-		'はなし','ひなた','ふつう','へちま','ほそい',
-		'まいく','みなみ','むすめ','めいし','すもう',
-		'やすみ','ゆみや','よわい',
-		'からい','ひるま','れきし','ろしあ',
+		'あした', 'いるか', 'うしろ', 'えすて', 'おうむ',
+		'からす', 'きつね', 'くるみ', 'けいと', 'こあら',
+		'さくら', 'しまね', 'すみれ', 'せなか', 'そふと',
+		'たぬき', 'ちくわ', 'つみき', 'てにす', 'といれ',
+		'なかみ', 'にもつ', 'ぬるい', 'ねむい',
+		'はなし', 'ひなた', 'ふつう', 'へちま', 'ほそい',
+		'まいく', 'みなみ', 'むすめ', 'めいし', 'すもう',
+		'やすみ', 'ゆみや', 'よわい',
+		'からい', 'ひるま', 'れきし', 'ろしあ',
 		'わるい'
 	];
 }
 
-else if(level == '3') {
+else if (level == '3') {
 	vocab = [
-		'けいこうとう','まかろん','ゆうえんち','あさくさえき','いすらえる',
-		'ういるす','えきたい','おつかれ','かすてら','きたきつね',
-		'くるまいす','けいさつ','こうこう','さいたま','しまうま',
-		'すいとう','せれくと','そういくふう','たいふう','ちかそうこ',
-		'ついとう','といあわせ','おはなみ','にちよう','ぬるまゆ',
-		'ねるまもおしむ','はいけい','ひるやすみ','ふろしき','へいかいしき',
-		'ほしうらない','まいにち','おみまい','むらさき','かきとめ',
-		'いもうと','やわらかい','ゆうめい','たいよう','おわらい',
-		'うるさい','れいあうと','おもしろい','わいやれす'
+		'けいこうとう', 'まかろん', 'ゆうえんち', 'あさくさえき', 'いすらえる',
+		'ういるす', 'えきたい', 'おつかれ', 'かすてら', 'きたきつね',
+		'くるまいす', 'けいさつ', 'こうこう', 'さいたま', 'しまうま',
+		'すいとう', 'せれくと', 'そういくふう', 'たいふう', 'ちかそうこ',
+		'ついとう', 'といあわせ', 'おはなみ', 'にちよう', 'ぬるまゆ',
+		'ねるまもおしむ', 'はいけい', 'ひるやすみ', 'ふろしき', 'へいかいしき',
+		'ほしうらない', 'まいにち', 'おみまい', 'むらさき', 'かきとめ',
+		'いもうと', 'やわらかい', 'ゆうめい', 'たいよう', 'おわらい',
+		'うるさい', 'れいあうと', 'おもしろい', 'わいやれす'
 	];
 }
 
-else location.href = 'index.html';  //URLがおかしい場合はメニューにとぶ
+else location.href = 'dashboard.html';  //URLがおかしい場合はメニューにとぶ
 
 const video = document.getElementById("video");
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
 
-let hand_model = null;  //手を検出するモデル
-let class_model = null;  //指文字を分類するモデル
-let tensor_image = null;  //入力画像の行列
+let hand_model = null;    // 手を検出するモデル
+let class_model = null;   // 指文字を分類するモデル
+let tensor_image = null;  // 入力画像の行列
+let cpText = null;        // 質問のテキスト
 
 const modelParams = {
-    flipHorizontal: true,  // flip e.g for video
-    maxNumBoxes: 1,        // maximum number of boxes to detect
-    iouThreshold: 0.5,     // ioU threshold for non-max suppression
-    scoreThreshold: 0.7,   // confidence threshold for predictions.
+	flipHorizontal: true,  // flip e.g for video
+	maxNumBoxes: 1,        // maximum number of boxes to detect
+	iouThreshold: 0.5,     // ioU threshold for non-max suppression
+	scoreThreshold: 0.7,   // confidence threshold for predictions.
 }
 
 //モデル読み込み
 handTrack.load(modelParams).then(l_model => {
-    hand_model = l_model;
+	hand_model = l_model;
 	load_class_model();
-	$("#status").text("Model Loaded !")
+	$("#status").text("準備完了 !");
 	startVideo();
 });
 
@@ -88,22 +91,24 @@ async function load_class_model() {
 
 //ビデオの開始
 function startVideo() {
-    handTrack.startVideo(video).then(function (status) {
+	handTrack.startVideo(video).then(function (status) {
 		$("#status").remove();
-		main();
-    });
+		cpText = vocab[Math.floor(Math.random() * Math.floor(vocab.length))];  //テキストはランダムで決める
+		$("#question").html(cpText.replace(/(\S)/g, "<span>$&</span>"));
+		main();  // メインループ
+	});
 }
 
 //メインの処理
 function main() {
-	classify(get_tensor());  //手を検出して指文字を分類する
-	requestAnimationFrame(main);  //繰り返し処理
+	classify(get_tensor());       // 手を検出して指文字を分類する
+	requestAnimationFrame(main);  // 繰り返し処理
 }
 
 //手だけを取得する
-function get_tensor(){
-    hand_model.detect(video).then(predictions => {
-        if(predictions.length != 0) {
+function get_tensor() {
+	hand_model.detect(video).then(predictions => {
+		if (predictions.length != 0) {
 			//枠の拡張
 			predictions[0]['bbox'][0] -= 50;
 			predictions[0]['bbox'][1] -= 50;
@@ -121,58 +126,55 @@ function get_tensor(){
 			tensor_image = preprocessImage(video);  //手を検出できなかった時はビデオを入力にする
 		}
 		hand_model.renderPredictions(predictions, canvas, context, video);  //枠の描画
-    });
-    return tensor_image;
+	});
+	return tensor_image;
 }
 
 //指文字の分類
-async function classify(tensor_image){
+async function classify(tensor_image) {
 	let prediction = await class_model.predict(tensor_image).data();
-	let results = Array.from(prediction).map(function(p,i){
-        return {probability: p,className: CLASSES[i]};
-	}).sort(function(a,b){
-		return b.probability-a.probability;
-	}).slice(0,5);
+	let results = Array.from(prediction).map(function (p, i) {
+		return { probability: p, className: CLASSES[i] };
+	}).sort(function (a, b) {
+		return b.probability - a.probability;
+	}).slice(0, 10);
 	other(results);
 };
 
 //前処理
-function preprocessImage(image){
-	let tensor = tf.browser.fromPixels(image).resizeNearestNeighbor([100,100]).toFloat();	
+function preprocessImage(image) {
+	let tensor = tf.browser.fromPixels(image).resizeNearestNeighbor([100, 100]).toFloat();
 	let offset = tf.scalar(255);
-    return tensor.div(offset).expandDims();
+	return tensor.div(offset).expandDims();
 }
 
 //その他の処理
 let status = true;  //処理が可能か判断する
 let score = 0;  //正解数
 let idx = 0;  //色を変える文字の番号
-let cpText = vocab[Math.floor(Math.random() * Math.floor(vocab.length))];  //テキストはランダムで決める
-//cpText='ち'
-$("#question").html(cpText.replace(/(\S)/g, "<span>$&</span>"));
-function other(results){
-	if(!status) return;
+function other(results) {
+	if (!status) return;
 	$("#console").empty();
-	results.forEach(function(p) {
+	results.forEach(function (p) {
 		//コンソールにクラスと確率を表示
-		$("#console").append('<div>・'+p.className+' : '+(p.probability*100).toFixed(2)+'%</div>');
-		if(p.probability.toFixed(2) > 0.01 && p.className == cpText[idx]){
-			$("#question span:nth-child(" + (idx+1) + ")").css({'color':'white'});
+		$("#console").append('<div>・' + p.className + ' : ' + p.probability.toFixed(3) + '</div>');
+		if (p.probability.toFixed(3) > 0.010 && p.className == cpText[idx]) {
+			$("#question span:nth-child(" + (idx + 1) + ")").css({ 'color': 'white' });
 			idx++;
 		}
-		if(idx == cpText.length) {
+		if (idx == cpText.length) {
 			score++;
 			idx = 0;  //番号をリセット
-			if(score == 3) {
+			if (score == 3) {
 				status = false;  //処理をやめる
 				swal.fire({
 					title: "レッスンクリア！",
 					icon: "success",
-				}).then(function() {
-					location.href = 'index.html';
+				}).then(function () {
+					location.href = 'dashboard.html';
 				});
 			}
-			else{
+			else {
 				swal.fire({
 					title: score + "/3",
 					icon: "success",
@@ -180,17 +182,16 @@ function other(results){
 					showConfirmButton: false,
 				});
 				cpText = vocab[Math.floor(Math.random() * Math.floor(vocab.length))];  //テキストはランダムで決める
-				//cpText='ち'
 				$("#question").html(cpText.replace(/(\S)/g, "<span>$&</span>"));
 			}
 		}
 	});
 }
 
-$("#yubimoji").click(function() {
-	$("#modal").fadeIn();  //モーダルを表示
+$("#yubimoji").click(function () {
+	$(".modal").fadeIn();  // モーダルを表示
 })
 
-$(".modal-close").click(function() {
-	$("#modal").fadeOut();  //モーダルを非表示
+$(".modal-close").click(function () {
+	$(".modal").fadeOut();  // モーダルを非表示
 })
